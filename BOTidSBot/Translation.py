f2d91555d3613762fcc9be633039a3b5faf904e2
bot <93746814+minarada@users.noreply.github.com>
2022-01-22 01:35:21 +0200

    Update Translation.py

--- BOTidSBot/Translation.py
+++ BOTidSBot/Translation.py
@@ -48,7 +48,11 @@ class Translation(object):
 """
 
     JOIN_TEXT = """
-📢 𝐉𝐨𝐢𝐧 𝐌𝐲 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 📢
+📢 𝗡𝗘𝗪 𝗬𝗢𝗥𝗞 ✈ 📢
+"""
+
+JOIN_TEXT = """
+𝗡𝗘𝗪 𝗬𝗢𝗥𝗞 ✈
 """
  
     TRY_TEXT = """
@@ -56,7 +60,14 @@ class Translation(object):
 """
 
     FSUB_TEXT = """
-📢 𝐉𝐨𝐢𝐧 𝐌𝐲 𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐓𝐡𝐞𝐧 𝐔𝐬𝐞 𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 📢
+🚸| عذرا عزيزي
+🔰| عليك الاشتراك بقناة البوت لتتمكن من استخدامه
+
+- https://t.me/us7a5
+
+-https://t.me/TE_NetFlix
+
+‼️| اشترك ثم ارسل /start
 """
 
     ID_TEXT = """
